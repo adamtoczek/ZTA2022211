52ec421511a527894fc5acdc26e2117bd01188ad
Adam Toczek <a.a.toczek@gmail.com>
2022-11-19 09:36:21 +0100

    selenium start

---
+++ src/main/java/day1/PrimeNumber.java
@@ -0,0 +1,16 @@
+package day1;/*
+napisz metode ktora sprawdza czy dana liczba jest liczba pierwsza
+liczby pierwsze to takie ktore dziela sie tylko przez 1 i sama siebie
+np 2,3,5,7,11,13,17,23, ...
+ */
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println("2 - "+isPrime(2));
+        System.out.println("4 - "+isPrime(4));
+    }
+
+    private static boolean isPrime(int i) {
+        return false;
+    }
+}
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BaseTest {
     public WebDriver driver;
 
