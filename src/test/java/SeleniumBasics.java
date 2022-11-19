c243eb67711a523c7028cb259ca69b366f1fd243
Adam Toczek <a.a.toczek@gmail.com>
2022-11-19 15:27:41 +0100

    selenium explicit wait

--- src/test/java/SeleniumBasics.java
+++ src/test/java/SeleniumBasics.java
@@ -2,9 +2,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -170,4 +173,12 @@ public class SeleniumBasics extends BaseTest{
         assertEquals("Well Done!", dbClick.getText().trim());
     }
 
+    @Test
+    public void explicitWaitTest() {
+        driver.get("https://automationtesting.co.uk/loader.html");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement btn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loaderBtn")));
+        btn.click();
+
+    }
 }
