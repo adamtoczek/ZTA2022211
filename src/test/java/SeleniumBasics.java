ed21989d1ab8a162221eb7b190502f8dd6679df2
Adam Toczek <a.a.toczek@gmail.com>
2022-11-19 09:59:47 +0100

    selenium formularz

--- src/test/java/SeleniumBasics.java
+++ src/test/java/SeleniumBasics.java
@@ -1,8 +1,25 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class SeleniumBasics extends BaseTest{
     @Test
-    public void test1(){
-        driver.get("http://146.59.32.4/index.php");
+    public void shouldFillForm() throws InterruptedException {
+        driver.get("http://146.59.32.4/index.php?controller=authentication&create_account=1");
+
+        driver.findElement(By.name("firstname")).sendKeys("Adam");
+
+
+        List<WebElement> radio =  driver.findElements(By.cssSelector(".radio-inline"));
+        for (WebElement r : radio) {
+            if (r.getText().equals("Mr."))
+                r.click();
+        }
+
+
+        Thread.sleep(5000);
+
     }
 }
