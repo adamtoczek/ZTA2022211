eea935fb7402b5f8332baa099fd710aaf6cdfdcb
Adam Toczek <a.a.toczek@gmail.com>
2022-11-20 11:30:58 +0100

    working spaghetti in POP

---
+++ src/test/java/MyStorePOPTests.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.Test;
+import pageObjects.BasePage;
+import pageObjects.HomePage;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyStorePOPTests extends BaseTest{
+    @Test
+    public void cartShouldContainAddedItems() throws InterruptedException {
+        int qty = 3;
+
+//        BasePage bp = new BasePage();
+        HomePage.navigateTo();
+        BasePage.homePage.openProduct("THE ADVENTURE POSTER");
+        BasePage.productPage.changeOption("Dimension", "80x120cm");
+        BasePage.productPage.changeQty(qty);
+        BasePage.productPage.addToBasket();
+
+        int curPrice = BasePage.productPage.getCurrentPrice();
+        assertEquals(TestUtils.formatPrice(curPrice), BasePage.cartPreview.getItemPrice());
+        assertEquals(TestUtils.formatPrice(curPrice*qty), BasePage.cartPreview.getSubtotal());
+
+        BasePage.cartPreview.clickProceedToCheckout();
+
+        assertEquals(TestUtils.formatPrice(curPrice*qty), BasePage.cartPage.getItemPrice());
+
+        Thread.sleep(3000);
+
+    }
+
+}
